206ab9f46e5c22b49f8f1b54ecf3e611f10ac566
jigneshbhimani <jigneshbhimani2411@gmail.com>
2022-04-24 10:49:10 +0530

    Speech Recognition using Python

--- speech.py
+++ speech.py
@@ -2,11 +2,11 @@ import speech_recognition as sr
 import pyaudio
 
 r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Speak Anything :")
-    audio = r.listen(source)
+with sr.Microphone() as source:                         # use the default microphone as the audio source
+    print("Speak Anything :")                           
+    audio = r.listen(source)                            # listen for the first phrase and extract it into audio data
     try:
-        text = r.recognize_google(audio)
+        text = r.recognize_google(audio)                # recognize speech using Google Speech Recognition
         print("You said : {}".format(text))
-    except:
+    except LookupError:                                 # speech is unintelligible
         print("Sorry could not recognize your voice")
